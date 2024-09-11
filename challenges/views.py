e5779735846ad12fa7a505ad1c74fa2b79552d42
megacodist <megacodist@gmail.com>
2024-09-11 20:56:07 +0430

    v2 - Developing

--- challenges/views.py
+++ challenges/views.py
@@ -2,13 +2,24 @@
 # 
 #
 
+from typing import Any
+
 from django.http import HttpRequest
 from django.http.response import HttpResponse
+from django.shortcuts import render
 
 
 def getChallengesIndexPage(request: HttpRequest) -> HttpResponse:
-    pass
+    context = dict[str, Any]()
+    return render(
+        request,
+        template_name='challenges-index.j2',
+        context=context)
 
 
 def getRpsPage(request: HttpRequest) -> HttpResponse:
-    pass
+    context = dict[str, Any]()
+    return render(
+        request,
+        template_name='rps.j2',
+        context=context)
