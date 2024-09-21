3669936be7fe5884f66d8556d7cc549f0fcfb043
megacodist <megacodist@gmail.com>
2024-09-21 13:42:26 +0330

    v2 - Developing

--- challenges/views.py
+++ challenges/views.py
@@ -7,6 +7,7 @@ from typing import Any
 from django.http import HttpRequest
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.template import engines
 from django.views.decorators.http import require_http_methods
 
 
@@ -20,10 +21,12 @@ def getChallengesIndexPage(request: HttpRequest) -> HttpResponse:
 
 @require_http_methods(['GET', 'POST'])
 def playRps(request: HttpRequest) -> HttpResponse: # type: ignore
+    from django.http import HttpResponse as HttpResponse_
     if request.method == 'GET':
-        return render(
-            request,
-            template_name='rps.j2',
-            context={},)
+        jinja2Engine = engines['jinja2']
+        template = jinja2Engine.get_template('challenges/rps/page.j2')
+        return HttpResponse_(template.render(
+            context={},
+            request=request,))
     if request.method == 'POST':
         pass
