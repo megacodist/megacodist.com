4109c4411abcbdecc8577aba9ce44e03e4a3b391
megacodist <megacodist@gmail.com>
2024-11-21 20:50:46 +0330

    Changing architecture

--- django/core/celery.py
+++ django/core/celery.py
@@ -10,9 +10,13 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 # Creating a Celery instance...
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
 celeryApp = Celery('core')
 
 # Loading task modules from all registered Django app configs...
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
 celeryApp.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Forcing Celery to auto-discover tasks from installed apps...
